0692c5a1e5109529581b4874d0047389e13aa9bd
Mark Chang <chang.m.mark@gmail.com>
2019-05-08 19:42:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers","hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningofArray(chocolateBars) {
-  chocolateBars.unshift
+  console.log()
 }
   function destructivelyAddElementToBeginningOfArray() {
     
